69f6eab13bb868569e68f29683d5f95ea01ce38a
dalvarez131 <dalvarez131@gmail.com>
2017-02-09 21:50:20 -0500

    Noveno ejercicio resuelto

--- challenges/9-triangle-side/my_solution.rb
+++ challenges/9-triangle-side/my_solution.rb
@@ -1,5 +1,15 @@
 # Triangle Side
 
 # Tu solucion abajo:
+def valid_triangle? (one,two,three)
+  if ((one >0)&&(two>0)&&(three>0))
+      if (((one+two)>=three)&&((two+three)>=one)&&((three+one)>=two))
+        true
+      else
+        false
+      end
+  else
+      false
+  end
 
-
+end
