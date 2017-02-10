c1a092aeacbd3134414cfc11cdd23637c460ce29
dalvarez131 <dalvarez131@gmail.com>
2017-02-09 20:28:08 -0500

    Octavo ejercicio resuelto

--- challenges/8-calculate-grade/my_solution.rb
+++ challenges/8-calculate-grade/my_solution.rb
@@ -2,3 +2,16 @@
 
 # Tu solucion abajo:
 
+def get_grade promedio
+  if (promedio <60)
+    "F"
+  elsif ((promedio >=60)&&(promedio<70))
+    "D"
+  elsif ((promedio >=70)&&(promedio<80))
+    "C"
+  elsif ((promedio >=80)&&(promedio<90))
+    "B"
+  elsif (promedio >=90)
+    "A"
+  end
+end
