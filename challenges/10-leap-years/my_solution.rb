4a2344f341111a07fbf0171a3f8273ab01f11897
dalvarez131 <dalvarez131@gmail.com>
2017-02-09 22:29:30 -0500

    Decimo Ejercicio Resuelto

--- challenges/10-leap-years/my_solution.rb
+++ challenges/10-leap-years/my_solution.rb
@@ -4,4 +4,16 @@
 
 
 # Tu solucion abajo:
-
+def leap_year? leap_number
+  if(leap_number % 4 == 0) && (leap_number % 100 == 0)&&(leap_number%400 ==0)
+    true
+  elsif (leap_number % 4 == 0) && (leap_number % 100 == 0)&&!(leap_number%400 ==0)
+    false
+  elsif (leap_number % 4 == 0) && (leap_number%400 ==0)
+    true
+  elsif (leap_number % 4 == 0) && !(leap_number % 100 == 0)
+    true
+  else
+    false
+  end
+end
