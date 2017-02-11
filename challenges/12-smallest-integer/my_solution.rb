1cf95df7e9c00865eccfa78c7ace378b8161a4c1
dalvarez131 <dalvarez131@gmail.com>
2017-02-10 20:59:36 -0500

    resuelto con seudocodigo

--- challenges/12-smallest-integer/my_solution.rb
+++ challenges/12-smallest-integer/my_solution.rb
@@ -1,7 +1,8 @@
 # Smallest Integer
-
-
-
+#1. input un arreglo de numeros
+#2. iterar sobre el arreglo
+#3. crear un variable voy a ir guardando el mas pequeño
+4. en cada iteracion comparar el elemento con
 # Tu solucion abajo:
 def smallest_integer pequeño
   pequeño.min
