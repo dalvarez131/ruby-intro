8d89d9791c9790fbbc444b69c0e08bacb2ec45e0
dalvarez131 <dalvarez131@gmail.com>
2017-02-14 13:22:26 -0500

    19 ejercicio resuelto

--- challenges/19-pretty-triangle/my_solution.rb
+++ challenges/19-pretty-triangle/my_solution.rb
@@ -3,3 +3,15 @@
 
 # Tu solucion abajo:
 
+def print_triangle int1
+  if int1<=0
+
+  else
+    otro=0
+    while (otro<int1)
+      otro += 1
+      h = Hash.new {|h,k| h[otro] = '*'*otro.to_i}   #=> {}
+      puts h.default(otro)
+    end
+  end
+end
