5ba4b7b81c0cd247251c97ec6e05dfc52214648a
dalvarez131 <dalvarez131@gmail.com>
2017-02-14 14:11:41 -0500

    20 ejericicio resuelto

--- challenges/20-factorial/my_solution.rb
+++ challenges/20-factorial/my_solution.rb
@@ -2,3 +2,18 @@
 
 
 # Tu solucion abajo:
+def factorial int1
+  if int1<0
+  else
+    factor=1
+    while int1>=0
+      if(int1==0)||(int1==1)
+        break
+      else
+        factor=factor*(int1)
+        int1=int1-1
+      end
+    end
+  end
+  p factor
+end
