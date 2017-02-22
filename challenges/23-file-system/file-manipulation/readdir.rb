a0b553c8d2120238ece213b2f314c49ed0e1357d
dalvarez131 <dalvarez131@gmail.com>
2017-02-22 18:05:07 -0500

    Solucion parcial de la 23

--- challenges/23-file-system/file-manipulation/readdir.rb
+++ challenges/23-file-system/file-manipulation/readdir.rb
@@ -1,1 +1,14 @@
 # Escribe aca el codigo para leer los contenidos de la carpeta testFiles, no debes leer los contenidos de la carpeta nesteada. El resultado final es que imprimas los contenidos en la consola.
+p test_files1=Dir.entries("testFiles")
+test_files1.each do |dir1|
+  puts
+  p "testFiles/#{dir1}"
+  puts
+  if !(File.directory?("testFiles/#{dir1}"))
+    File.readlines("testFiles/#{dir1}").each do |line|
+      puts line
+    end
+    puts
+    puts
+  end
+end
--- challenges/23-file-system/file-manipulation/write_file.rb
+++ challenges/23-file-system/file-manipulation/write_file.rb
@@ -1,1 +1,8 @@
-
+if (ARGV.index("--phrase")==nil)
+  p 'Algo salio mal'
+else
+  mensaje=ARGV[(ARGV.index("--phrase"))+1]
+  def write_file (mensaje)
+    File.open("testFiles/nestedDirectory/soy_nuevo.md", "a+") { |file| file.write(mensaje) }
+  end
+end
