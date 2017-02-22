4a69b5b5f2077cd085ab19db480d7481f12772d3
dalvarez131 <dalvarez131@gmail.com>
2017-02-21 20:32:44 -0500

    23 Ejercicio clases resuelto

---
+++ challenges/23-file-system/clases/argv.rb
@@ -0,0 +1,7 @@
+if (ARGV.index("--user")==nil||ARGV.index("--greeting")==nil)
+  p 'Algo salio mal'
+else
+  index1=(ARGV.index("--user"))+1
+  index2=(ARGV.index("--greeting"))+1
+  p "Hola #{ARGV[index1]}, #{ARGV[index2]}"
+end
---
+++ challenges/23-file-system/clases/filename.rb
@@ -0,0 +1,6 @@
+require "pathname"
+
+url1=Pathname.new("filename.rb")
+puts "Hola mundo desde #{url1}"
+
+#puts File.basename(__FILE__)
