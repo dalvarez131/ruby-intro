f383be66f137588d49723fcc9e98ca7b17aa0a1a
dalvarez131 <dalvarez131@gmail.com>
2017-02-23 20:13:05 -0500

    Ejercicios resueltos

--- challenges/25-words-in-file/my_solution.rb
+++ challenges/25-words-in-file/my_solution.rb
@@ -1,14 +1,41 @@
 # Pseudocode
 
-
+# most_common_words('some_file.txt', 3)
+# > 'apples': 18 occurrences
+# > 'oranges': 13 occurrences
+# > 'melons': 9 occurrences
 
 # 1. Solucion Inicial
+def most_common_words(filename, number_words)
+  archivo1=File.open("./#{filename}", "r") do |file|
+    text= file.gets(nil,49773)
+    hash1= Hash.new(0)
+    iteraciones=0
+    reserved_words=['ref','and','by','his','that','with','to','in','an','of','the','is','a','s','was','this','it','from','for','or','p','on','her','are']
+
+    result=text.scan /\w+/
+    result=result.map do |word|
+       word.downcase!
+       word
+    end
+    result.each do |word|
+      if (reserved_words.include?(word))
+        result.delete(word)
+      end
+    end
+    result.each do |word|
+       iteraciones=result.count(word)
+       hash1[word]=iteraciones
+    end
+    array2=hash1.max_by(number_words) { |word,number_occ| number_occ }
+    array2.each do |word, number_occ|
+      puts "'#{word}': #{number_occ} occurrences"
+    end
+  end
+end
 
 
-
-
-
-
+most_common_words('source.txt', 20)
 # 2. Solucion con Refactor
 
 
--- challenges/26-pig-latin/my_solution.rb
+++ challenges/26-pig-latin/my_solution.rb
@@ -1,12 +1,37 @@
-# Pseudocode
+Pseudocode
+1. Solucion Inicial
 
+def pig_latin (string1)
+  if (/\A[aeiouAEIOU]/===string1[0])
+    return string1
+  else
+    array1= string1.partition(/[aeiouAEIOU]/)
+    first=array1.shift
+    array1<<first+'ay'
+    p string2=array1.join.downcase.capitalize
+  end
+end
 
+pig_latin('Primero')
 
-# 1. Solucion Inicial
+def pig_latin_tense (string1)
+  tense=string1.split(' ')
+  result=tense.map do |element|
+    if (/\A[aeiouAEIOU]/===element)
+      element
+    else
+      array1= element.partition(/[aeiouAEIOU]/)
+      first=array1.shift
+      array1<<first+'ay'
+      string2=array1.join.downcase
+    end
+  end
+  result[0].capitalize!
+  result.join(' ')
 
+end
 
-
-
+p pig_latin_tense('Hola Pepito como estas')
 
 
 # 2. Solucion con Refactor
