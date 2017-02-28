a067d676a3df3d3757f07cd375f7edcb54fad68d
dalvarez131 <dalvarez131@gmail.com>
2017-02-27 20:58:49 -0500

    Ejercicio 27 resuelto

--- challenges/27-OOP/die-class/my_solution.rb
+++ challenges/27-OOP/die-class/my_solution.rb
@@ -4,6 +4,24 @@
 
 # 1. Solucion Inicial
 
+class Die
+  def initialize (sides)
+    if sides < 1
+      raise ArgumentError.new("Only numbers are allowed")
+    else
+      @sides=sides
+    end
+  end
+  def sides
+    @sides
+  end
+  def roll
+    rand(1..@sides)
+  end
+end
+dado=Die.new(5)
+dado.sides
+dado.roll
 
 
 
